edde25b2b37aa87a55a7e7f460181ca3a4b2b5eb
ProfOnestone <profonestone@gmail.com>
2022-03-31 23:57:16 +0200

    Fix: UI Add: HexString

---
+++ EnhancedColor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Colorado
+{
+    public class EnhancedColor :INotifyPropertyChanged
+    {
+        private System.Windows.Media.Color _WinColor;
+        public System.Windows.Media.Color WinColor
+        { 
+            get 
+            { 
+                return _WinColor; 
+            } 
+            set 
+            { 
+                _WinColor = value;
+                this.BrushColor = new SolidColorBrush(value);
+                NotifyPropertyChanged("RedHex");
+                NotifyPropertyChanged("GreenHex");
+                NotifyPropertyChanged("BlueHex");
+                NotifyPropertyChanged("HexColor");
+            } 
+        }
+
+        public string RedHex { get { return _WinColor.R.ToString(); } }
+        public string GreenHex { get { return _WinColor.G.ToString(); } }
+        public string BlueHex { get { return _WinColor.B.ToString(); } }
+        public string HexColor { get { return "#" + _WinColor.R.ToString("X2") + _WinColor.G.ToString("X2") + _WinColor.B.ToString("X2"); } }
+
+        private System.Windows.Media.SolidColorBrush _BrushColor;
+        public System.Windows.Media.SolidColorBrush BrushColor { get { return _BrushColor; } set { _BrushColor = value; NotifyPropertyChanged();  } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,193 +24,71 @@ namespace Colorado
         [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
         public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
 
-        thecolor test = new thecolor();
+        EnhancedColor currentColor = new EnhancedColor();
+        EnhancedColor snappedColor = new EnhancedColor();
 
+        //creating 1x1 pixel
         Bitmap screenPixel = new Bitmap(1, 1, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
         public MainWindow()
         {
-             
             InitializeComponent();
 
-            System.Windows.Media.Color mc = getcolorfromposition();
+            currentColor.WinColor = GetColorAtCursorPosition();
 
-            test.actualcolor = new SolidColorBrush(mc);
-            test.valuered = mc.R.ToString();
-            test.valuegreen = mc.G.ToString();
-            test.valueblue = mc.B.ToString();
+            this.DataContext = currentColor;
 
-            this.DataContext = test;
+            InitializeColorReadTimer();
+        }
 
-            System.Windows.Threading.DispatcherTimer mytimer= new System.Windows.Threading.DispatcherTimer();
-            mytimer.Tick += Mytimer_Tick;
-            mytimer.Interval = new TimeSpan(0, 0, 0, 0,200);
+        private void InitializeColorReadTimer()
+        {
+            System.Windows.Threading.DispatcherTimer mytimer = new System.Windows.Threading.DispatcherTimer();
+            mytimer.Tick += ColorReadTick;
+            mytimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
             mytimer.Start();
-
-            load_colorlist();
-
         }
 
-        private void Mytimer_Tick(object sender, EventArgs e)
+        private void ColorReadTick(object sender, EventArgs e)
         {
-            if (!TheList.IsMouseOver)
-            {
-                System.Windows.Media.Color mcc = getcolorfromposition();
-
-                test.actualcolor = new SolidColorBrush(mcc);
-                test.valuered = mcc.R.ToString();
-                test.valuegreen = mcc.G.ToString();
-                test.valueblue = mcc.B.ToString();
-
-            }
-
+            currentColor.WinColor = GetColorAtCursorPosition();
         }
 
-        private System.Windows.Media.Color getcolorfromposition()
+        private System.Windows.Media.Color GetColorAtCursorPosition()
         {
-            System.Drawing.Point cursor = new System.Drawing.Point();
-            GetCursorPos(ref cursor);
-            System.Drawing.Color dc = GetColorAt(cursor);
-            System.Windows.Media.Color mc = System.Windows.Media.Color.FromArgb(dc.A, dc.R, dc.G, dc.B);
-
-            return mc;
-        }
-
-        private System.Drawing.Color GetColorAt(System.Drawing.Point location)
-        {
+            System.Drawing.Point cursorPosition = new System.Drawing.Point();
+            GetCursorPos(ref cursorPosition);
             using (Graphics gdest = Graphics.FromImage(screenPixel))
             {
                 using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
                 {
                     IntPtr hSrcDC = gsrc.GetHdc();
                     IntPtr hDC = gdest.GetHdc();
-                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, cursorPosition.X, cursorPosition.Y, (int)CopyPixelOperation.SourceCopy);
                     gdest.ReleaseHdc();
                     gsrc.ReleaseHdc();
                 }
             }
-
-            return screenPixel.GetPixel(0, 0);
+            System.Drawing.Color drawingColor = screenPixel.GetPixel(0, 0);
+            System.Windows.Media.Color mediaColor = ConvertToMediaColor(drawingColor);
+            return mediaColor;
         }
-
-        private void lbl_color_SourceUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
+        private System.Windows.Media.Color ConvertToMediaColor(System.Drawing.Color drawingColor)
         {
-
+            System.Windows.Media.Color mc = System.Windows.Media.Color.FromArgb(drawingColor.A, drawingColor.R, drawingColor.G, drawingColor.B);
+            return mc;
         }
-
         private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            if(e.Key == System.Windows.Input.Key.S)
+            if (e.Key == System.Windows.Input.Key.S)
             {
-                string hex_code = test.actualcolor.ToString();
-                StreamWriter mysw = File.AppendText(@"D:\Visual Studio\C#\Colorado\Colorado\knowncolours.txt");
-                mysw.WriteLine(hex_code);
-                mysw.Dispose();
-            }
-            load_colorlist();
-            
-
-        }
-
-        private void load_colorlist()
-        {
-            List<string> colorhexes = new List<string>();
-            string[] listofcolours = File.ReadAllLines(@"D:\Visual Studio\C#\Colorado\Colorado\knowncolours.txt",System.Text.Encoding.UTF8);
-            foreach (var item in listofcolours)
-            {
-                
-                colorhexes.Add(item.ToString());
-            }
-
-
-
-            TheList.ItemsSource = colorhexes;
-
-
-
-        }
-
-        private void TheList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            test.actualcolor = (SolidColorBrush)(new BrushConverter().ConvertFrom((sender as System.Windows.Controls.ListBox).SelectedItem));
-            test.valuered = test.actualcolor.Color.R.ToString();
-            test.valuegreen = test.actualcolor.Color.B.ToString(); ;
-            test.valueblue = test.actualcolor.Color.G.ToString(); ;
-
-
-        }
-    }
-
-
-
-
-
-    //-------------------------------------------------------Color-Klasse-------------------------------------------------------------//
-
-    public class thecolor : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private SolidColorBrush _actualcolor;
-        public SolidColorBrush actualcolor
-        {
-            get { return _actualcolor; }          
-            set {
-                _actualcolor = value;
-                OnPropertyChanged("actualcolor");
-            }
-        }
-
-        private string _valuered;
-        public string valuered
-        {
-            get { return _valuered; }
-            set
-            {
-                _valuered = value;
-                OnPropertyChanged("valuered");
-            }
-        }
-
-        private string _valuegreen;
-        public string valuegreen
-        {
-            get { return _valuegreen; }
-            set
-            {
-                _valuegreen = value;
-                OnPropertyChanged("valuegreen");
-            }
-        }
-
-        private string _valueblue;
-        public string valueblue
-        {
-            get { return _valueblue; }
-            set
-            {
-                _valueblue = value;
-                OnPropertyChanged("valueblue");
-            }
-        }
-
-
-
-
-        protected void OnPropertyChanged(string name)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(name));
+                snappedColor = currentColor;
+                tb_snappedHex.Text = snappedColor.HexColor;
+                lbl_snappedBrush.Background = snappedColor.BrushColor;
+                tb_snappedRed.Text = snappedColor.RedHex;
+                tb_snappedBlue.Text = snappedColor.BlueHex;
+                tb_snappedGreen.Text = snappedColor.GreenHex;
             }
         }
     }
-
-
-
-
-
-  
-
 }
